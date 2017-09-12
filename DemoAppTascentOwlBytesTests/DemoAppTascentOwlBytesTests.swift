4def63a0f5ee7fa6648367eae546e5b6e3df9508
Pietro Degrazia <pietrodegrazia@hotmail.com>
2017-09-11 23:20:35 -0300

    Initial Commit

---
+++ DemoAppTascentOwlBytesTests/DemoAppTascentOwlBytesTests.swift
@@ -0,0 +1,36 @@
+//
+//  DemoAppTascentOwlBytesTests.swift
+//  DemoAppTascentOwlBytesTests
+//
+//  Created by Pietro Degrazia on 9/11/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import XCTest
+@testable import DemoAppTascentOwlBytes
+
+class DemoAppTascentOwlBytesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
